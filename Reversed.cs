41e0a681fccd0cbdd9647956de330122ac163550
RMiller28 <Millerrycki5@gmail.com>
2014-05-07 09:45:30 -0400

    First

---
+++ Reversed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reversed.cs
+{
+    class Program
+    {
+        public static string Reversed(string input)
+        {
+            string output = "";
+
+            char[] seq = input.ToCharArray(); 
+
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+
+            string input = Console.ReadLine();
+            string output = Reversed(input);
+
+            Console.WriteLine(output);
+        }
+    }
+}
